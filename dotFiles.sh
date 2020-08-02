f61c8c1840b2576557ef58adff8a66e3e6b1151f
lucas <l.martin.rosello@gmail.com>
2020-08-02 01:59:10 -0300

    agrego google chrome

--- dotFiles.sh
+++ dotFiles.sh
@@ -60,6 +60,11 @@ apt install apache2 -y
 chmod -R 777 /var/www/html
 rm /var/www/html/index.html
 
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
 
 
 ## LENGUAJES ##
