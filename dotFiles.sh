c13f59270c28026d231379eb5f999c56f2245254
lucas <l.martin.rosello@gmail.com>
2020-08-02 23:38:14 -0300

    agrego robo3t

--- dotFiles.sh
+++ dotFiles.sh
@@ -65,6 +65,9 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
+# Robo3T
+snap install robo3t-snap
+
 
 
 ## LENGUAJES ##
