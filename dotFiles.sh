a007bffc73553acdef6d1d4851551c95fcf11519
lucas <l.martin.rosello@gmail.com>
2020-08-24 18:05:19 -0300

    agrego bin/bash

--- dotFiles.sh
+++ dotFiles.sh
@@ -1,5 +1,5 @@
-# #!/bin/bash
-# # -*- ENCODING: UTF-8 -*-
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
 
 
 echo '  _'            
--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo -ne '\n' | ssh-keygen -t rsa -b 4096 -C "l.martin.rosello@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
