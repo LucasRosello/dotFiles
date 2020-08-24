91daf885a533779c04c7053ca76b4fd5ae88b8f0
lucas <l.martin.rosello@gmail.com>
2020-08-23 21:42:06 -0300

    termino mi generador de llaves SSH

--- ssh.sh
+++ ssh.sh
@@ -1,10 +1,10 @@
-# echo -ne '\n' | ssh-keygen -t rsa -b 4096 -C "l.martin.rosello@gmail.com"
+echo -ne '\n' | ssh-keygen -t rsa -b 4096 -C "l.martin.rosello@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
 
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/id_rsa
+KEY=$(cat ~/.ssh/id_rsa.pub)
+FECHA=$(date)
 
-# curl -u "l.martin.rosello@gmail.com" --data '{"title":"llaveSSH","key":"ssh-rsa AAAAM3JkZ+xlDS7IZw== l.martin.rosello@gmail.com"}' https://api.github.com/user/keys
+curl -u "l.martin.rosello@gmail.com" --data '{"title":"'"$FECHA"'","key":"'"$KEY"'"}' https://api.github.com/user/keys
 
-# cat ~/.ssh/id_rsa.pub
-
-# git config --global user.email "l.martin.rosello@gmail.com"
+git config --global user.email "l.martin.rosello@gmail.com"
