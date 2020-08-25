0ac8fc8b9e3f0a3b49d2c169f5e8caa06614fe29
lucas <l.martin.rosello@gmail.com>
2020-08-24 22:51:11 -0300

    añado guake por afuera

---
+++ Scripts/0base.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Guake
+apt install guake -y
+ln -s /usr/share/applications/guake.desktop /etc/xdg/autostart
+
+exit
--- echo.sh
+++ echo.sh
@@ -3,3 +3,5 @@
 echo "corriendo";
 
 exit;
+
+#wget -O - https://raw.githubusercontent.com/LucasRosello/dotFiles/master/echo.sh | bash
