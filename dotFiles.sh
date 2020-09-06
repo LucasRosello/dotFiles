0bc4de295a0d0f0de5e7cf8cba2326ac40fdd14a
lucas <l.martin.rosello@gmail.com>
2020-09-06 18:43:25 -0300

    agrego audacity

--- dotFiles.sh
+++ dotFiles.sh
@@ -130,6 +130,9 @@ alias gowww='cd /home/lucas/go/src/github.com/customer-experience'
 # Spotify
 snap install spotify
 
+# Audacity
+sudo apt-get install audacity -y
+
 # Ssh
 wget -O - https://raw.githubusercontent.com/LucasRosello/dotFiles/master/Scripts/Ssh.sh | bash
 
