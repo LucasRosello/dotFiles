6b44067fc5176f90bd955b391b0145bff3b9366c
lucas <l.martin.rosello@gmail.com>
2020-09-13 15:42:23 -0300

    arreglo estructura en dotfiles de docker

--- Scripts/Docker.sh
+++ Scripts/Docker.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+
+# Docker
 apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update
 apt install docker-ce -y
+
+
+exit
